0de575e6d9b631464bd1fdff1d3d0d792f9c7945
archercode <tonnyquintos@gmail.com>
2015-01-03 18:25:26 +0800

    initial commit

---
+++ src/com/example/bluetoothandroid/MainActivityBluetooth.java
@@ -0,0 +1,130 @@
+package com.example.bluetoothandroid;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivityBluetooth extends Activity {
+ private static final String TAG = "bluetooth1";
+   private static final int REQUEST_CODE = 1234;
+   private BluetoothAdapter btAdapter = null;
+   private BluetoothSocket btSocket = null;
+   private OutputStream outStream = null;
+   ArrayList i ; 
+    
+   // SPP UUID service 
+   private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+  
+   // MAC-address of Bluetooth module 
+   private static String address = "00:14:02:27:50:60";
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main_activity_bluetooth);
+  tryToConnect();
+ }
+ 
+ public void tryToConnect()
+ {
+  
+  btAdapter = BluetoothAdapter.getDefaultAdapter();
+     
+  checkBTState();//To check whwther the bluetooth is on,else turn on the bluetooth
+    
+     BluetoothDevice device = btAdapter.getRemoteDevice(address);
+  
+    
+  try {
+   btSocket = createBluetoothSocket(device);
+  } catch (IOException e1) {
+   errorExit("Fatal Error", "In onResume() and socket create failed: " + e1.getMessage() + ".");
+  }
+        
+     btAdapter.cancelDiscovery();
+     Log.d(TAG, "...Connecting...");
+     try {
+       btSocket.connect();
+       Log.d(TAG, "...Connection ok...");
+     } catch (IOException e) {
+       try {
+         btSocket.close();
+       } catch (IOException e2) {
+         errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+       }
+     }
+      
+    // btSocket.isConnected();
+     
+     Toast.makeText(getBaseContext(), "Connected", Toast.LENGTH_LONG).show();
+     // Create a data stream so we can talk to server.
+     Log.d(TAG, "...Create Socket...");
+  
+     try {
+       outStream = btSocket.getOutputStream();
+     } catch (IOException e) {
+       errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+    }  
+ } 
+   private void sendData(String message) {
+      byte[] msgBuffer = message.getBytes();   
+      Log.d(TAG, "...Send data: " + message + "...");   
+      try {
+        outStream.write(msgBuffer);
+      } catch (IOException e) {         
+      }
+    }
+   
+   private void checkBTState() {
+      // Check for Bluetooth support and then check to make sure it is turned on
+      // Emulator doesn't support Bluetooth and will return null
+      if(btAdapter==null) { 
+        errorExit("Fatal Error", "Bluetooth not support");
+      } else {
+        if (btAdapter.isEnabled()) {
+          Log.d(TAG, "...Bluetooth ON...");
+        } else {
+          //Prompt user to turn on Bluetooth
+          Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+          startActivityForResult(enableBtIntent, 1);
+        }
+      }
+    }
+   
+   private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+       if(Build.VERSION.SDK_INT >= 10){
+           try {
+               final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+               return (BluetoothSocket) m.invoke(device, MY_UUID);
+           } catch (Exception e) {
+               Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+           }
+       }
+       return  device.createRfcommSocketToServiceRecord(MY_UUID);
+   }
+   private void errorExit(String title, String message){
+      Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+      finish();
+    }
+   
+  public void LedOn(View View)
+  {
+   sendData("1");
+  }
+  public void LedOff(View View)
+  {
+   sendData("0");
+  }
+}
